5514e56da74d8f509a9cc9e6338ee387031d6375
WITS.RoxusDai <RoxusDai@wistronits.com>
2022-10-25 15:56:45 +0800

    arrange activity code

--- app/src/main/java/com/example/room_test/NewsActivity.kt
+++ app/src/main/java/com/example/room_test/NewsActivity.kt
@@ -14,10 +14,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.room_test.databinding.ActivityNewsBinding
 
 
-class NewsActivity : AppCompatActivity(){
+class NewsActivity : AppCompatActivity() {
     private lateinit var newsBinding: ActivityNewsBinding
     lateinit var newsAdapter: NewsAdapter
-    lateinit var newsDataSourceFactory : DataSourceFactory
+    lateinit var newsDataSourceFactory: DataSourceFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,9 +25,10 @@ class NewsActivity : AppCompatActivity(){
         newsDataSourceFactory = DataSourceFactory(this, newsdatasource)
         //var livePagedListBuilder = LivePagedListBuilder(newsDataSourceFactory, newsdatasource.getConfig())
         //var mLiveData: LiveData<PagedList<List<RoomEntity>>> = livePagedListBuilder.build()
-        newsAdapter = NewsAdapter(this,newsdatasource)
-        newsBinding.rvList.adapter = newsAdapter
+        newsAdapter = NewsAdapter(this, newsdatasource)
+
         newsBinding = ActivityNewsBinding.inflate(LayoutInflater.from(this))
+        newsBinding.rvList.adapter = newsAdapter
         newsBinding.rvList.layoutManager = LinearLayoutManager(this)
         newsBinding.rvList.itemAnimator = null
 
